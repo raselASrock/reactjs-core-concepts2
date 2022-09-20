0a8d4415d85d2b66b1094add7470aa70e0bb4f8b
Rasel <mdraselm413@gmail.com>
2022-09-20 19:09:55 +0600

    recap

--- src/App.js
+++ src/App.js
@@ -2,24 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products = [
+    {name: 'Laptop', price: '47,000', brand: 'Lenovo'},
+    {name: 'Phone', price: '7,000', brand: 'Realme'},
+    {name: 'Tablet', price: '17,000', brand: 'Samsung'},
+    {name: 'Watch', price: '3,000', brand: 'GoPro'}
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        products.map(product => <Product name = {product.name} price = {product.price} brand = {product.brand}></Product>)
+      }
+      {/* <Product name ="Laptop" price = "47,000" brand = 'Lenovo'></Product>
+      <Product name ="Phone" price = "75,000" brand = 'Realme'></Product>
+      <Product name ="Watch" price = "3,500" brand = 'GoPro'></Product> */}
     </div>
   );
+}
+
+function Product(props){
+  return(
+    <div className='product'>
+      <h3>Name:{props.name}</h3>
+      <p>Price:{props.price}</p>
+      <p>Brand:{props.brand}</p>
+    </div>
+  )
 }
 
 export default App;
